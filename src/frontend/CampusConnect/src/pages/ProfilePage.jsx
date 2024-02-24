fffe57dbecfe2bbf2311015e63286f842e0b8645
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2024-02-24 16:52:04 +0530

    refactored

--- src/frontend/CampusConnect/src/pages/ProfilePage.jsx
+++ src/frontend/CampusConnect/src/pages/ProfilePage.jsx
@@ -1,7 +1,25 @@
 function ProfilePage() {
   return (
     <>
-      <div>ProfilePage</div>;<form action=""></form>
+      <div>ProfilePage</div>
+      <form>
+        <input placeholder="username" type="text" />
+        <select name="" id="">
+          <option value="">select role</option>
+          <option value="">Student</option>
+          <option value="">Principal</option>
+          <option value="">Hod</option>
+          <option value="">Admin</option>
+        </select>
+        <br />
+        <select name="" id="">
+          <option value="">select Department</option>
+          <option value="">IT</option>
+          <option value="">Comps</option>
+          <option value="">Extc</option>
+          <option value="">Civil</option>
+        </select>
+      </form>
     </>
   );
 }
